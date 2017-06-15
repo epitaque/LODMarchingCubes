0a791c0fc3a1db74917b863bd630e0f7a1732652
Brian Hoy <brianhoy4@gmail.com>
2017-06-15 09:58:07 -0400

    progress

--- Assets/MC/SurfaceExtractor.cs
+++ Assets/MC/SurfaceExtractor.cs
@@ -220,7 +220,22 @@ public static class SurfaceExtractor {
             new Vector3[] {
                 new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f),
                 new Vector3(0f, -1f, 1f), new Vector3(1f, -1f, 1f), new Vector3(1f, 1f, 1f), new Vector3(0f, 0f, 1f)
+            }
+        },
+
+        new Vector3[][] { // 3 sides
+            new Vector3[] {
+                new Vector3(-1f, -1f, -1f), new Vector3(1f, -1f, -1f), new Vector3(1f, 1f, -1f), new Vector3(-1f, 1f, -1f),
+                new Vector3(-1f, -1f, 0f), new Vector3(0f, -1f, 0f), new Vector3(1f, 1f, 1f), new Vector3(0f, 0f, 0f)
             },
+            new Vector3[] {
+                new Vector3(0f, -1f, -1f), new Vector3(1f, -1f, -1f), new Vector3(1f, 1f, -1f), new Vector3(0f, 0f, -1f),
+                new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f)
+            },
+            new Vector3[] {
+                new Vector3(0f, -1f, 0f), new Vector3(0f, -1f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f),
+                new Vector3(0f, -1f, 1f), new Vector3(1f, -1f, 1f), new Vector3(1f, 1f, 1f), new Vector3(0f, 0f, 1f)
+            }
         }
     };
 
